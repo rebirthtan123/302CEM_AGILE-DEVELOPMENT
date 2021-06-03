44744f4cb7cf014d2bc7916c3d823e17f56d8d97
tanweijian828 <tweijian28@gmail.com>
2021-06-03 13:43:07 +0800

    RestaurantOrderingSystem
    Resubmit for changing zip file to folder

---
+++ RestaurantOrderingSystem/login.blade.php
@@ -0,0 +1,6 @@
+<h1>User Login</h1>
+<form action="user" method="POST">
+	<input type="text" name="user" placeholder="enter username"> <br> <br>
+	<input type="password" name="password" placeholder="enter user password"> <br> <br>
+	<button type="submit">Login</button>
+</form>
