e3603784d12af6c749943ae3a1ca83995bec7a42
Lim Sing Seng <sslim0287@gmail.com>
2021-06-03 12:37:09 +0800

    Login function v0.1.1
    Resubmit for changing zip file to folder

---
+++ Login function v0.1.1/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaffController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/login',[StaffController::class,'login'])->name('auth.login');
+Route::post('auth/check', [StaffController::class,'check'])->name('auth.check');
