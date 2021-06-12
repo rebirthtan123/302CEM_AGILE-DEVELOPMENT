2ff8a41807e3a877f448a0ae9a6d772c1dbeb8c5
rebirthtan123 <75563705+rebirthtan123@users.noreply.github.com>
2021-06-12 21:26:58 +0800

    reconfigure

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get("staff/table",[TableController::class,'index']);
 
 Route::get("staff/addOrder/{id}",[OrderController::class,'index'])->name('staff.addOrder');
 Route::post("staff/addOrder/store/{id}",[OrderController::class,'store'])->name('staff.addOrder.store');
-Route::post("staff/mainmenu/delete/{id}",[OrderController::class,'destroy'])->name('staff.delete');
+Route::post("staff/mainmenu/delete/{id}",[OrderController::class,'delete'])->name('staff.delete');
 
 Route::get("staff/kitchen",[KitchenController::class,'index']);
 Route::get("staff/edit/{id}",[KitchenController::class,'showOut']);
